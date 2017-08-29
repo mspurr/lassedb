cc0d7fceb1f3467b8c0e66feec3e06d23457d51a
mspurr <scitus@hotmail.dk>
2017-08-29 03:51:48 +0200

    company page still working on it

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -69,6 +69,6 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:title, :description, :price, :mcap, :focus, :logo)
+      params.require(:company).permit(:title, :description, :price, :mcap, :focus, :logo, :ticker)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828215324) do
+ActiveRecord::Schema.define(version: 20170829004006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170828215324) do
     t.text     "focus"
     t.text     "description"
     t.string   "logo"
+    t.string   "ticker"
   end
 
 end

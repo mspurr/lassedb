7f02af229224f91a9c17d5499b102e76e9b2ad84
mspurr <scitus@hotmail.dk>
2017-08-31 00:09:57 +0200

    added charts and data for all companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -71,6 +71,62 @@ class CompaniesController < ApplicationController
       @stock = @stock.split.join(' ')[2..-1]
       @stock = JSON.parse @stock
       @stock = @stock[0]
+      # data from google as csv for charts
+      url = 'https://www.google.com/finance/getprices?q=' + @company.GTicker + '&x=' + @company.exch + '&i=86400&p=40Y&f=d,c,v,k,o,h,l&df=cpct&auto=0&ei=Ef6XUYDfCqSTiAKEMg'
+      @data = Faraday.get url
+      @data = @data.body
+
+      @data = @data.lines[7..-1].join
+      @data = @data.split("\n")
+      @date = (@data[0][0..9]).to_i
+
+      @data2 = @data
+      @data3 = []
+      n = 0
+      x = 0
+      set = []
+
+      while n < @data2.length
+        d = @data2[x].split(',')
+        if d.length < 2
+          set.append(@data2[0..x])
+          @data2 = @data2[x+1..-1]
+          x=0
+          n=0
+        else
+          @data3.append(d)
+          x = x+1
+          n = n + 1
+        end
+        #@data2[x][0] = x*86400 + @date
+      end
+
+      n = 0
+      x = 0
+      curr_d = 0
+      while n < @data3.length
+        if @data3[n][0].length == 11
+          @data3[n][0] = @data3[n][0][1..-1]
+          curr_d = @data3[n][0].to_i
+          @data3[n][0] = @data3[n][0].to_i
+        else
+          @data3[n][0] = @data3[n][0].to_i*86400 + curr_d
+        end
+        @data3[n][0] = Time.at(@data3[n][0]).strftime("20%y-%m-%d")
+        n = n+1
+      end
+
+      n = 0
+      a = "["
+      while n < @data3.length do
+        h = "{\"date\": \""+ @data3[n][0] +"\", \"open\": \""+ @data3[n][4] +"\", \"high\": \""+ @data3[n][2] +"\", \"low\": \""+ @data3[n][3] +"\", \"close\": \""+ @data3[n][1] +"\"}"
+        a = a + h + ", "
+        n = n+1
+      end
+      @a = a[0..-3] + "]"
+
+
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

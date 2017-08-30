8e7aba0e33c364cb127cdc89729a0fe37e21eb48
mspurr <scitus@hotmail.dk>
2017-08-31 01:10:14 +0200

    added stock price changes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -125,6 +125,20 @@ class CompaniesController < ApplicationController
       end
       @a = a[0..-3] + "]"
 
+      # Monthly change
+      @month = ((@data3[-1][1]).to_f-(@data3[-22][1]).to_f)/((@data3[-22][1]).to_f)*100
+      #weekly change
+      @week = ((@data3[-1][1]).to_f-(@data3[-6][1]).to_f)/((@data3[-6][1]).to_f)*100
+      #year change
+      @year = ((@data3[-1][1]).to_f-(@data3[-250][1]).to_f)/((@data3[-250][1]).to_f)*100
+      #3year change
+      @tyear = ((@data3[-1][1]).to_f-(@data3[-250*3][1]).to_f)/((@data3[-250*3][1]).to_f)*100
+      #3year change
+      @fyear = ((@data3[-1][1]).to_f-(@data3[-250*5][1]).to_f)/((@data3[-250*5][1]).to_f)*100
+      #year to date
+      now = @data3[-1][0][5..6].to_i
+      @dyear = ((@data3[-1][1]).to_f-(@data3[-now*21+1][1]).to_f)/((@data3[-now*21+1][1]).to_f)*100
+
 
 
     end
